aa666ee830250dccf4b7e43865d54ada41f2e3d0
Erin Spruenken <spruenke@hu-berlin.de>
2021-11-06 01:52:10 +0100

    d

--- Old_dont_use/test_cpp.R
+++ Old_dont_use/test_cpp.R
@@ -33,30 +33,61 @@ settings$wald = 0
 settings$anv  = 0
 settings$maxt = 0
 
-z = 3
+z = 10
 
 sets = settings[z,]
-sizes = nm_gen(nn = sets$nn, n_i = sets$n_i, m_ij = sets$m_ij, each_s = sets$each_s, both_s = sets$both_s, identical_s = sets$identical_s, identical_c = sets$identical_c)
+sizes = rankCluster::nm_gen(nn = sets$nn, n_i = sets$n_i, m_ij = sets$m_ij, each_s = sets$each_s, both_s = sets$both_s, identical_s = sets$identical_s, identical_c = sets$identical_c)
 
-f2 = sum(sizes[[1]]) - length(sizes[[1]])
+f2 = sqrt(sum(sizes[[1]]) - length(sizes[[1]]))
 
-c_mat = contrMat(sizes[[1]], type = c_type) %*% diag(1, length(sizes[[1]]))
+c_mat = multcomp::contrMat(sizes[[1]], type = c_type) %*% diag(1, length(sizes[[1]]))
 dec = matrix(0, nrow = 3, ncol = nsim)
 theta = rep(1/sets$nn, length(sizes[[1]]))
 psi = NULL
 
 data_n = h_0_f(sizes[[1]], sizes[[2]], dist = dist_c, corstruct = "independent", rho = settings$rho[z], params = dist_params)
 
-f_theta_p(c(0.2, 0.4), data_n)
-f_theta_p2(c(0.2, 0.4), data_n)
-if(is.null(psi)){
-  psi = list()
-  for(i in 1:length(data_n)){
-    psi[[i]] = round(rep(1 / length(data_n[[i]]), length(data_n[[i]])), 7)
-  }
+# f_theta_p(c(0.2, 0.4), data_n)
+# f_theta_p2(c(0.2, 0.4), data_n)
+# if(is.null(psi)){
+#   psi = list()
+#   for(i in 1:length(data_n)){
+#     psi[[i]] = round(rep(1 / length(data_n[[i]]), length(data_n[[i]])), 7)
+#   }
+# }
+
+max_T2  = function(n, data, p_null = 0.5, cont, normal = FALSE, theta = NULL, psi = NULL, alpha){
+  Sigma = rankCluster::sigma_est(n, data, theta = theta, psi = psi)
+  p = rankCluster::rel_eff(data, theta, psi)
+  R = cov2cor(Sigma)
+  R_c = cov2cor(cont%*%Sigma%*%t(cont))
+  stat = sqrt(g(n)) * (p - p_null) / sqrt(diag(Sigma))
+  #if(normal == TRUE) crit = mvtnorm::qmvnorm(1-alpha, tail = "lower.tail", mean = rep(0, length(p)), corr = R)$quantile
+  #if(normal == FALSE) crit = mvtnorm::qmvt(1-alpha, tail = "lower.tail", df = g(n) - 1, corr = R)$quantile
+  if(normal == FALSE) rej = 1-mvtnorm::pmvt(upper = abs(stat), df = g(n) - 1, corr = R)
+  #dec = max(abs(stat)) > crit
+  #return(list(Statistic = stat, df = g(n) - 1, reject = dec))
+  
+  return(rej < alpha/2)
 }
 
-rel_eff(data_n)
+rankCluster::rel_eff(data_n)
+
+ab = replicate(1000, expr = {
+  data_b = rankCluster::h_0_f(sizes[[1]], sizes[[2]], dist = dist_c, corstruct = "independent", rho = settings$rho[z], params = dist_params)
+  
+  # f_theta_p(c(0.2, 0.4), data_n)
+  # f_theta_p2(c(0.2, 0.4), data_n)
+  # if(is.null(psi)){
+  #   psi = list()
+  #   for(i in 1:length(data_n)){
+  #     psi[[i]] = round(rep(1 / length(data_n[[i]]), length(data_n[[i]])), 7)
+  #   }
+  # }
+  
+  c(rankCluster::q_wald(sizes[[1]], data_b, cont=c_mat, alpha = 0.05)$reject,
+    rankCluster::q_anova(sizes[[1]], data_b, cont = c_mat, f_2 = f2, alpha = 0.05)$reject)
+})
 rel_eff_p(data_n)
 rel_eff_p2(data_n)
 
@@ -65,6 +96,31 @@ nn = rep(3, 5)
 A_1 = sigma_est(sizes[[1]], data_n, theta = theta, psi = NULL)
 A_2 = sigma_est_p2(sizes[[1]], data_n, theta = NULL, psi = NULL)
 A_3 = debug_p(sizes[[1]], data_n, theta = NULL, psi = NULL)
+
+st = Sys.time()
+rankCluster::max_T(sizes[[1]], data_n, cont = c_mat, alpha = 0.05)$reject
+Sys.time() - st
+st = Sys.time()
+max_T2(sizes[[1]], data_n, cont = c_mat, alpha = 0.05)
+Sys.time() - st
+
+microbenchmark::microbenchmark(rankCluster::max_T(sizes[[1]], data_n, cont=c_mat, alpha = 0.05)$reject,
+                               rankCluster::max_T2(sizes[[1]], data_n, cont = c_mat, alpha = 0.05)$reject)
+ab = replicate(1000, expr = {
+  data_b = rankCluster::h_0_f(sizes[[1]], sizes[[2]], dist = dist_c, corstruct = "independent", rho = settings$rho[z], params = dist_params)
+  
+  # f_theta_p(c(0.2, 0.4), data_n)
+  # f_theta_p2(c(0.2, 0.4), data_n)
+  # if(is.null(psi)){
+  #   psi = list()
+  #   for(i in 1:length(data_n)){
+  #     psi[[i]] = round(rep(1 / length(data_n[[i]]), length(data_n[[i]])), 7)
+  #   }
+  # }
+  
+  c(rankCluster::max_T(sizes[[1]], data_b, cont=c_mat, alpha = 0.05)$reject,
+    rankCluster::max_T2(sizes[[1]], data_b, cont = c_mat, alpha = 0.05)$reject)
+})
 # C++ vs R ----------------------------------------------------------------
 
 sim_fun2 = function(){
@@ -145,3 +201,22 @@ stat3 = function(p, M, Sigma){
 # Q_Wald
 # Q_Anova
 # max_t
+abb = replicate(1000, expr = {
+data_n = h_0_f(sizes[[1]], sizes[[2]], dist = dist_c, corstruct = "independent", rho = settings$rho[z], params = dist_params)
+pnull = 0.5
+alpha = 0.05
+n = sizes[[1]]
+sig = rankCluster::sigma_est(n, data_n, theta)
+phat = rankCluster::rel_eff(data_n, theta)
+Rhat = cov2cor(sig)
+R_c  = cov2cor(c_mat%*%sig%*%t(c_mat))
+stat = sqrt(rankCluster::g(n)) * (phat - pnull) / sqrt(diag(sig))
+crit = mvtnorm::qmvt(1-alpha/2, tail = "lower.tail", df = rankCluster::g(n) - 1, corr = Rhat)$quantile
+c(1-mvtnorm::pmvt(upper=rep(max(abs(stat)), nrow(Rhat)), df = rankCluster::g(n) - 1, corr = Rhat, keepAttr = F),
+max(abs(stat)) > crit)
+
+})
+
+abb1 = which(abb[1,] <= alpha/2)
+abb2 = which(abb[2,] == T)
+all(abb1 == abb2)
--- settings.R
+++ settings.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # number of Samples nn = 3, 5, 10, 25
 # Identical Sample Size n_i = 3, 5, 10, 15, 25, 50, 100, 1000
 
@@ -74,3 +75,81 @@ both_s = c(F, T)
       samples = samples[-rem_ind,]
 
 rm(nm_1, nm_2, nm_3, nm_4, both_s, each_s, m_ij, n_i, nn)
+=======
+# number of Samples nn = 3, 5, 10, 25
+# Identical Sample Size n_i = 3, 5, 10, 15, 25, 50, 100, 1000
+
+# Identical Cluster Size m_ij = 3, 5, 10, 15, 20, 25, 40
+
+# One Large, One Small Cluster m_ij = 10, 15, 20, 25; m_ik = 3; m_ih = 40
+
+# One Large, One Small Sample n_i = 10, 15, 25, 50; n_j = 3; n_k = 150
+
+# Identical Cluster Size m_ij = 3, 5, 10, 15, 20, 25, 40
+
+# One Large, One Small Cluster m_ij = 10, 15, 20, 25; m_ik = 3; m_ih = 40
+
+# number of samples
+nn = c(3, 5, 10)
+each_s = c(F, T)
+both_s = c(F, T)
+
+# Identical Sample Sizes --------------------------------------------------
+    n_i = c(3, 5, 10, 15, 25)
+
+  # Identical Cluster Size
+
+      m_ij = c(3, 5, 10, 15)
+      nm_1 = expand.grid(nn, n_i, m_ij)
+      nm_1$each_s = F
+      nm_1$both_s = F
+      nm_1$identical_s = T
+      nm_1$identical_c = T
+      nm_1$grp = 1
+      colnames(nm_1)[1:3] = c("nn", "n_i", "m_ij")
+
+  # Large/Small Cluster
+      m_ij = c(5, 10)
+      nm_2 = expand.grid(nn, n_i, m_ij, each_s, both_s)
+      nm_2$identical_s = T
+      nm_2$identical_c = F
+      colnames(nm_2)[1:5] = c("nn", "n_i", "m_ij", "each_s", "both_s")
+      # Remove invalid condition
+      #nm_2 = nm_2[-which(which(nm_2$each_s == T) %in% which(nm_2$both_s == F)),]
+      nm_2$grp = 2
+
+
+# Large/Small Sample Size -------------------------------------------------
+
+    n_i = c(5, 10, 15)
+
+  # Identical Cluster Size
+
+      m_ij = c(3, 5, 10, 15)
+      nm_3 = expand.grid(nn, n_i, m_ij)
+      nm_3$each_s = F
+      nm_3$both_s = F
+      nm_3$identical_s = F
+      nm_3$identical_c = T
+      nm_3$grp = 3
+      colnames(nm_3)[1:3] = c("nn", "n_i", "m_ij")
+
+  # Large/Small Cluster
+      m_ij = c(5, 10)
+      nm_4 = expand.grid(nn, n_i, m_ij, each_s, both_s)
+      nm_4$identical_s = F
+      nm_4$identical_c = F
+      colnames(nm_4)[1:5] = c("nn", "n_i", "m_ij", "each_s", "both_s")
+      #nm_4 = nm_4[-which(which(nm_4$each_s == T) %in% which(nm_4$both_s == F)),]
+      nm_4$grp = 4
+
+
+# Bind --------------------------------------------------------------------
+
+    samples = rbind(nm_1, nm_2, nm_3, nm_4)
+      rem_ind = intersect(which(samples$each_s == T), which(samples$both_s ==F))
+      
+      samples = samples[-rem_ind,]
+
+rm(nm_1, nm_2, nm_3, nm_4, both_s, each_s, m_ij, n_i, nn)
+>>>>>>> Stashed changes
