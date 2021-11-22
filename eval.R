64d9a474ff1dfa480d86517146eec40bba78e234
Erin Spruenken <spruenke@hu-berlin.de>
2021-11-23 00:54:04 +0100

    Update eval.R

--- eval.R
+++ eval.R
@@ -3,45 +3,57 @@ library(car)
 library(ggplot2)
 library(ggpubr)
 
-load("norm_results.RData")
 
-# No Correlation
-no_cor = results[[1]]
+dists = c("norm", "pois", "beta", "binom")
+grps = c(1:4)
+res_sets = expand.grid(grps, dists, stringsAsFactors = F)
+names_sets = expand.grid(c("no_cor", "mid_cor", "sev_cor"), grps, dists, stringsAsFactors = F)
+#load("norm_results.RData")
 
-  # grp 1, 2, 3, 4
-  no_cor_1 = subset(no_cor, grp == 1)
-  val = c(no_cor_1$wald, no_cor_1$anv, no_cor_1$maxt)
-  no_cor_11 = as.data.frame(rbind(no_cor_1[,c(1:3)], no_cor_1[,c(1:3)], no_cor_1[,c(1:3)]))
-  no_cor_11$val = val
-  no_cor_11$typ = rep(c("Wald", "ANOVA", "MCTP"), each = nrow(no_cor_1))
-  # nn= 2, 3, 4, 5
-  p1 = ggplot(data = subset(no_cor_11, nn == 2))  +
-    geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = typ), size = 1) +
-    #geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = "dashed"))+
-    scale_colour_discrete(name = "Sample Size") +
-    scale_linetype_discrete(name = "Procedure") +
-    labs(title ="Type-I Error for 2 samples", x = "Clustersize", y = "Type-I error")
-  
-  p2 = ggplot(data = subset(no_cor_11, nn == 3))  +
-    geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = typ), size = 1) +
-    #geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = "dashed"))+
-    scale_colour_discrete(name = "Sample Size") +
-    scale_linetype_discrete(name = "Procedure") +
-    labs(title ="Type-I Error for 3 samples", x = "Clustersize", y = "Type-I error")
-  
-  p3 = ggplot(data = subset(no_cor_11, nn == 4))  +
-    geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = typ), size = 1) +
-    #geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = "dashed"))+
-    scale_colour_discrete(name = "Sample Size") +
-    scale_linetype_discrete(name = "Procedure") +
-    labs(title ="Type-I Error for 4 samples", x = "Clustersize", y = "Type-I error")
-  
-  p4 = ggplot(data = subset(no_cor_11, nn == 5))  +
-    geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = typ), size = 1) +
-    #geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = "dashed"))+
-    scale_colour_discrete(name = "Sample Size") +
-    scale_linetype_discrete(name = "Procedure") +
-    labs(title ="Type-I Error for 5 samples", x = "Clustersize", y = "Type-I error")
-  
-  p_nocor_11 = ggarrange(p1, p2, p3, p4, ncol=2, nrow=2, common.legend = TRUE, legend="right")
-  
+res = c()
+for(i in 1:nrow(res_sets)){
+# grp 1, 2, 3, 4
+  load(paste0(res_sets[i,2], "_results.RData"))
+    A = list()
+    for(j in 1:3){
+      dat = results[[j]]
+      dat_1 = subset(dat, grp == res_sets[i,1])
+      val = c(dat_1$wald, dat_1$anv, dat_1$maxt)
+      dat_11 = as.data.frame(rbind(dat_1[,c(1:3)], dat_1[,c(1:3)], dat_1[,c(1:3)]))
+      dat_11$val = val
+      dat_11$typ = rep(c("Wald", "ANOVA", "MCTP"), each = nrow(dat_1))
+      # nn= 2, 3, 4, 5
+      p1 = ggplot(data = subset(dat_11, nn == 2))  +
+        geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = typ), size = 1) +
+        #geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = "dashed"))+
+        scale_colour_discrete(name = "Sample Size") +
+        scale_linetype_discrete(name = "Procedure") +
+        labs(title ="Type-I Error for 2 samples", x = "Clustersize", y = "Type-I error")
+      
+      p2 = ggplot(data = subset(dat_11, nn == 3))  +
+        geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = typ), size = 1) +
+        #geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = "dashed"))+
+        scale_colour_discrete(name = "Sample Size") +
+        scale_linetype_discrete(name = "Procedure") +
+        labs(title ="Type-I Error for 3 samples", x = "Clustersize", y = "Type-I error")
+      
+      p3 = ggplot(data = subset(dat_11, nn == 4))  +
+        geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = typ), size = 1) +
+        #geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = "dashed"))+
+        scale_colour_discrete(name = "Sample Size") +
+        scale_linetype_discrete(name = "Procedure") +
+        labs(title ="Type-I Error for 4 samples", x = "Clustersize", y = "Type-I error")
+      
+      p4 = ggplot(data = subset(dat_11, nn == 5))  +
+        geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = typ), size = 1) +
+        #geom_line(aes(x = m_ij, y = val, colour = as.factor(n_i), linetype = "dashed"))+
+        scale_colour_discrete(name = "Sample Size") +
+        scale_linetype_discrete(name = "Procedure") +
+        labs(title ="Type-I Error for 5 samples", x = "Clustersize", y = "Type-I error")
+      
+      p_dat_11 = ggarrange(p1, p2, p3, p4, ncol=2, nrow=2, common.legend = TRUE, legend="right")
+      A[[j]] = p_dat_11
+      rm(p1, p2, p3, p4)
+    }
+  res = c(res, A)
+}
