ab9388d9783356867ec82f333e28ca1b9daf8cae
Matthias Becher <matthias.becher2193@gmail.com>
2021-12-10 14:56:27 +0100

    Fixed a typo in Settings.R which lead to an error in rbind(nm_5, nm_6)

--- settings.R
+++ settings.R
@@ -102,7 +102,7 @@ rm(nm_1, nm_2, nm_3, nm_4, both_s, each_s, m_ij, n_i)
   nm_6$identical_c = F
   nm_6$each_s = T
   nm_6$both_s = T
-  colnames(nm_5)[1:5] = c("nn", "n_i", "m_ij", "each_s", "both_s")
+  colnames(nm_6)[1:5] = c("nn", "n_i", "m_ij", "each_s", "both_s")
   #nm_4 = nm_4[-which(which(nm_4$each_s == T) %in% which(nm_4$both_s == F)),]
   nm_6$grp = 6
 
